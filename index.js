f3c632e92c640268ab909f7f4d6a578a0992727c
[madisonseal1] <madisonseal@icloud.com>
2026-04-14 21:21:37 -0400

    test

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const app = express();
 app.use(
     cors({
       credentials: true,
-      origin: process.env.CLIENT_URL || "http://localhost:3000",
+      origin: true, 
+      // origin: process.env.CLIENT_URL || "http://localhost:3000",
     })
    ); 
    const sessionOptions = {
